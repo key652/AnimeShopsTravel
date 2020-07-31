ba1c44c12e48b831d0d8de9c455213eb195a535d
key <rizegotiusa0130@gmail.com>
2020-07-31 23:18:37 +0900

    レイアウト改善

--- AnimeShopsTravel/View/ContributionView.swift
+++ AnimeShopsTravel/View/ContributionView.swift
@@ -38,10 +38,13 @@ class ContributionView: UIView {
         
         userNameLabel.adjustsFontSizeToFitWidth = true
         cameraButton.setImage(UIImage(named: "Camera"), for: .normal)
+        commentTextView.font = UIFont.systemFont(ofSize: 17)
         contributionButton.setTitle("投稿", for: .normal)
         contributionButton.setTitleColor(UIColor.white, for: .normal)
         contributionButton.backgroundColor = CustomColor.mainColor
         contributionButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        cancelButton.setTitle("キャンセル", for: .normal)
+        cancelButton.setTitleColor(UIColor.black, for: .normal)
         
     }
     
@@ -54,38 +57,38 @@ class ContributionView: UIView {
         cameraButton.translatesAutoresizingMaskIntoConstraints = false
         contributionButton.translatesAutoresizingMaskIntoConstraints = false
         
-        cancelButton.topAnchor.constraint(equalTo: topAnchor, constant: 40).isActive = true
-        cancelButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15).isActive = true
+        cancelButton.topAnchor.constraint(equalTo: topAnchor, constant: 30).isActive = true
+        cancelButton.leftAnchor.constraint(equalTo: leftAnchor, constant: 15).isActive = true
         cancelButton.heightAnchor.constraint(equalToConstant: 45).isActive = true
         cancelButton.widthAnchor.constraint(equalToConstant: 88).isActive = true
         
         profileImageView.topAnchor.constraint(equalTo: cancelButton.bottomAnchor, constant: 24).isActive = true
-        profileImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
+        profileImageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 20).isActive = true
         profileImageView.heightAnchor.constraint(equalToConstant: 60).isActive = true
         profileImageView.widthAnchor.constraint(equalToConstant: 60).isActive = true
         
         userNameLabel.topAnchor.constraint(equalTo: cancelButton.bottomAnchor, constant: 24).isActive = true
-        userNameLabel.leadingAnchor.constraint(equalTo: profileImageView.rightAnchor, constant: 8).isActive = true
-        userNameLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -20).isActive = true
+        userNameLabel.leftAnchor.constraint(equalTo: profileImageView.rightAnchor, constant: 10).isActive = true
+        userNameLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: 20).isActive = true
         userNameLabel.heightAnchor.constraint(equalToConstant: 60).isActive = true
         
-        commentTextView.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 28).isActive = true
-        commentTextView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
+        commentTextView.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 28).isActive = true
+        commentTextView.leftAnchor.constraint(equalTo: leftAnchor, constant: 20).isActive = true
         commentTextView.rightAnchor.constraint(equalTo: rightAnchor, constant: -20).isActive = true
-        commentTextView.heightAnchor.constraint(equalToConstant: 114).isActive = true
+        commentTextView.heightAnchor.constraint(equalToConstant: 130).isActive = true
         
-        contentImageView.topAnchor.constraint(equalTo: commentTextView.bottomAnchor, constant: 35).isActive = true
-        contentImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32).isActive = true
+        contentImageView.topAnchor.constraint(equalTo: commentTextView.bottomAnchor, constant: 10).isActive = true
+        contentImageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 32).isActive = true
         contentImageView.rightAnchor.constraint(equalTo: rightAnchor, constant: -32).isActive = true
-        contentImageView.heightAnchor.constraint(equalToConstant: 260).isActive = true
+        contentImageView.heightAnchor.constraint(equalToConstant: 250).isActive = true
         
         contributionButton.rightAnchor.constraint(equalTo: rightAnchor, constant: -20).isActive = true
-        contributionButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -38).isActive = true
-        contributionButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        contributionButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -35).isActive = true
         contributionButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        contributionButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
         
-        cameraButton.rightAnchor.constraint(equalTo: contributionButton.leadingAnchor, constant: -24).isActive = true
-        cameraButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -38).isActive = true
+        cameraButton.rightAnchor.constraint(equalTo: contributionButton.leftAnchor, constant: -24).isActive = true
+        cameraButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -35).isActive = true
         cameraButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         cameraButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
         
