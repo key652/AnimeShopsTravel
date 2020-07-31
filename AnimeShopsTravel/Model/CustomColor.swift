aba58599ccce23b69c47a0c3ffa7afac409c16bd
key <rizegotiusa0130@gmail.com>
2020-07-31 23:14:46 +0900

    使用するカラーを追加

--- AnimeShopsTravel/Model/CustomColor.swift
+++ AnimeShopsTravel/Model/CustomColor.swift
@@ -9,6 +9,7 @@ import UIKit
 
 struct CustomColor {
     static let mainColor = UIColor(red: 0.27, green: 0.74, blue: 0.70, alpha: 1)
+    static let logoutButtonColor = UIColor(red: 0.92, green: 0.40, blue: 0.44, alpha: 1)
 }
 
 class BlockButton: UIButton {
