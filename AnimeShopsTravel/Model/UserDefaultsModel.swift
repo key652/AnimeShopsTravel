a7978beb320998d0a50272236df5f0d85addec69
key <rizegotiusa0130@gmail.com>
2020-08-02 04:26:40 +0900

    コード追加(プロフィール変更処理)

--- AnimeShopsTravel/Model/UserDefaultsModel.swift
+++ AnimeShopsTravel/Model/UserDefaultsModel.swift
@@ -6,16 +6,21 @@
 //  Copyright © 2020 yuma yada. All rights reserved.
 //
 import Foundation
+import UIKit
 
 protocol UserDefaultsDelegate: class {
     func getProfileImageData() -> Data
     func getMyUserName() -> String
     func getMyUid() -> String
     func removeLoginToken()
+    func createLoginToken()
+    func changeProfileData(userName:String, profileImageData: Data, viewController: UIViewController)
 }
 
 
 class UserDefaultsModel: UserDefaultsDelegate {
+    private let alert = AlertCreateView()
+    
     func getProfileImageData() -> Data {
         var userprofileImageData: Data!
         if UserDefaults.standard.object(forKey: "profileImageData") != nil {
@@ -50,5 +55,16 @@ class UserDefaultsModel: UserDefaultsDelegate {
         UserDefaults.standard.removeObject(forKey: "loginToken")
     }
     
+    func changeProfileData(userName:String, profileImageData: Data, viewController: UIViewController) {
+        if UserDefaults.standard.object(forKey: "userName") != nil {
+            UserDefaults.standard.removeObject(forKey: "userName")
+        }
+        if UserDefaults.standard.object(forKey: "profileImageData") != nil {
+            UserDefaults.standard.removeObject(forKey: "profileImageData")
+        }
+        UserDefaults.standard.set(userName, forKey: "userName")
+        UserDefaults.standard.set(profileImageData, forKey: "profileImageData")
+        alert.alertCreate(title: "プロフィールを更新しました", message: "", actionTitle: "OK", viewCotroller: viewController)
+    }
     
 }
